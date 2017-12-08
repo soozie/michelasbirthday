5d874321ef564fb32d08143c57b778db527c97c5
Susanna Zandonai <susanna.zandonai@gmail.com>
2017-12-08 01:26:22 +0000

    Test Github io

---
+++ script.js
@@ -0,0 +1,34 @@
+const removeAllH1Classes = () => {
+  var h1s = document.getElementsByClassName('happyBirthday');
+
+  Array.from(h1s).forEach(function(h1){
+    $(h1).removeClass('active');
+  });
+}
+
+const startAnimationInterval = () => {
+  let i = 0;
+  const h1s = document.getElementsByClassName('happyBirthday');
+  $(h1s[i]).addClass('active');
+
+  setInterval(() => {
+    console.log('TIC');
+    removeAllH1Classes();
+    i++;
+    if (i < h1s.length) {
+      $(h1s[i]).addClass('active');
+    } else {
+      i = 0;
+      $(h1s[i]).addClass('active');
+    }
+  }, 1500);
+}
+
+$(document).ready(() => {
+  removeAllH1Classes();
+  startAnimationInterval();
+
+  $('#mail-button').on('click', () => {
+    window.open('https://mail.google.com', '_blank');
+  });
+});
