25738edb2e7774d3b3d2935e3c676396bd307e32
soozie <susanna.zandonai@gmail.com>
2017-12-08 08:12:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ $(document).ready(() => {
   startAnimationInterval();
 
   $('#mail-button').on('click', () => {
-    window.open('https://mail.google.com/', '_blank');
+    window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin', '_blank');
   });
 });
